3bcaa18f8d18d743ec217b4c1daccc035a0ad169
andy.boot <bootandy@gmail.com>
2016-10-14 23:44:33 +0100

    Read config from setup.cfg
    import style and line length read in from config file as well as
    specified on command line

--- shell.py
+++ shell.py
@@ -3,6 +3,8 @@ from __future__ import absolute_import, division, print_function
 import argparse
 import os
 
+import configparser
+
 from imps.core import Sorter, Style
 
 
@@ -11,7 +13,7 @@ def get_style(s):
         return Style.SMARKETS
     elif s in ('g', 'google'):
         return Style.GOOGLE
-    elif s in ('c', 'cryptography'):
+    elif s in ('c', 'crypto', 'cryptography'):
         return Style.CRYPTOGRAPHY
     else:
         raise Exception('Unknown style type %s', s)
@@ -39,15 +41,56 @@ def recurse_down_tree(sorter, path):
                     run(sorter, file_path)
 
 
+def get_config(path):
+    if os.path.isfile(path):
+        path, _ = os.path.split(path)
+
+    config = configparser.ConfigParser()
+
+    # handle no config file ever found
+    while not config.sections():
+        config.read(os.path.join(path, 'setup.cfg'))
+        path = os.path.join(path, '..')
+
+    return config
+
+
+def setup_vars(config, args):
+    # Read from command line first. Else setup.cfg 'imps' else 'flake8'. Else assume 's'
+    style = args.style
+    if not style:
+        style = config.get('imps', 'style', fallback=None)
+    if not style:
+        style = config.get('flake8', 'import-order-style', fallback=None)
+    if not style:
+        style = 's'
+
+    max_line_length = args.max_line_length
+    if not max_line_length:
+        max_line_length = config.get('imps', 'max-line-length', fallback=None)
+    if not max_line_length:
+        max_line_length = config.get('flake8', 'max-line-length', fallback=None)
+    if not max_line_length:
+        max_line_length = 80
+
+    return style, max_line_length
+
+
+
 def get_args():
     parser = argparse.ArgumentParser(description='Sort your python')
 
     parser.add_argument('file', nargs='?', default=os.getcwd())
-    parser.add_argument('-s', '--style', type=str, help='Import style', default='s')
+
+    parser.add_argument('-s', '--style', type=str, help='Import style', default='')
+    parser.add_argument('-l', '--max-line-length', type=int, help='Line length')
 
     args = parser.parse_args()
 
-    sorter = Sorter(get_style(args.style))
+    config = get_config(args.file)
+    style, max_line_length = setup_vars(config, args)
+
+    sorter = Sorter(get_style(style))
 
     recurse_down_tree(sorter, args.file)
 
