ec2b7e968ba7c791f852852bee34bc2dfc4101cd
andy.boot <bootandy@gmail.com>
2017-06-09 23:53:07 +0100

    fix flake8

--- imps/tests/test_shell.py
+++ imps/tests/test_shell.py
@@ -1,13 +1,9 @@
 from __future__ import absolute_import, division, print_function
 
-import pytest
-
 from imps.shell import normalize_file_name
 
 
 def test_normalize_file_name():
     assert normalize_file_name('tox.ini') == 'tox.ini'
     assert normalize_file_name('tox.ini:34:144') == 'tox.ini'
-
-
     assert normalize_file_name('bad_path.nothing') is None
