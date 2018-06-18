d71c8d1364217970fc76b5de2b45aa8c0a432e93
andy.boot <bootandy@gmail.com>
2018-06-18 23:54:37 +0100

    setup.py: new version. support py3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='imps',
-    version='0.2.5',
+    version='0.2.6',
     description='Python utility to sort Python imports',
     author='Andy Boot',
     author_email='bootandy@gmail.com',
@@ -21,7 +21,11 @@ setup(
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'Environment :: Console',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries',
         'Topic :: Utilities',
     ]
